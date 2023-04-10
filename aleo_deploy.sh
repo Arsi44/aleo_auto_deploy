84578f0ce5380769faca587c6edcbf62f5ed183d
arsi44 <gjenferd23@gmail.com>
2023-04-10 15:59:29 +0300

    fix

--- aleo_deploy.sh
+++ aleo_deploy.sh
@@ -1,34 +1,25 @@
 #!/bin/bash
 
-echo Enter your Private Key: && read PK && \
-echo Enter your View Key: && read VK && \
-echo Enter your Address: && read ADDRESS && \
-echo Paste the link: && read QUOTE_LINK && \
-echo Enter the Name of your contract "(any)": && read NAME
-
-
-if [[ $1 == "true" ]]; then
-  echo "Выполняем команду apt update"
-  apt update && apt install make clang pkg-config libssl-dev build-essential gcc xz-utils git curl vim tmux ntp jq llvm ufw -y
-
-  cd $HOME
-  git clone https://github.com/AleoHQ/snarkOS.git --depth 1
-  cd snarkOS
-  bash ./build_ubuntu.sh
-  source $HOME/.bashrc
-  source $HOME/.cargo/env
-
-  cd $HOME
-  git clone https://github.com/AleoHQ/leo.git
-  cd leo
-  cargo install --path .
-else
-  echo "Установка зависимосте не требуется"
+# если количетво аргументов не равно 5
+if [[ "$#" -ne 5 ]]; then
+  echo "Неверное количество аргументов"
+  exit 1
 fi
+
+PK="$1"
+VK="$2"
+ADDRESS="$3"
+QUOTE_LINK="$4"
+NAME="$5"
+
+echo "Private Key: $PK"
+echo "View Key: $VK"
+echo "Address: $ADDRESS"
+echo "Quote Link: $QUOTE_LINK"
+echo "Contract Name: $NAME"
 
 cd $HOME
 rm -rf leo_deploy
-
 
 cd $HOME && mkdir leo_deploy && cd leo_deploy
 leo new $NAME
@@ -43,3 +34,5 @@ snarkos developer deploy "$NAME.aleo" \
 --broadcast "https://vm.aleo.org/api/testnet3/transaction/broadcast" \
 --fee 600000 \
 --record "$RECORD"
+
+# ./aleo_deploy.sh APrivateKey1zkp3ckEYE8edcuGD917b8Dwv1LmdLjye3QtAmvJs5aaMwMT AViewKey1rnZzU1BaapGzu3rZSrUq6KHoefTZHKN1tQkaZtvvSJRN aleo1l3h53jde5uxy0v08w6zdwxsv754lmjcg3pwlnttd2smzwlf0syxs79rxe5 https://vm.aleo.org/api/testnet3/transaction/at1ll0njuj7nrr2uptjee0dedm62eufamwuqhqwp097ck5fsr3tkcgqa57quz… sobriquet213
